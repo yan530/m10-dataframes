c94e632513b9f9b289e7c53d40845d461a854523
Anni Yan <Anni Yan>
2017-04-11 15:10:25 -0700

    lol

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,46 +1,50 @@
 # Exercise 3: Working with Data Frames
 
 # Load R's "USPersonalExpenditure" dataest using the `data()` function
-
+data("USPersonalExpenditure")
 
 # The variable USPersonalExpenditure is now accessible to you. Unfortunately, it's not a data.frame
 # Test this using the is.data.frame function
-
+is.data.frame(expenditure)
 
 # Luckily, you can simply pass the USPersonalExpenditure variable to the data.frame function
 # to convert it a data.farme
+data.frame(USPersonalExpenditure)
 
 # Create a new variable by passing the USPersonalExpenditure to the data.frame function
-
+us.exp <- data.frame(data.frame(USPersonalExpenditure), stringsAsFactors=FALSE)
 
 # What are the column names of your dataframe?
-
+colnames(us.exp)
 
 # Why are they so strange?
 
 
 # What are the row names of your dataframe?
-
+rownames(us.exp)
 
 # Create a column `category` that is equal to your rownames
-
+us.exp$category <- rownames(us.exp)
 
 # How much money was spent on personal care in 1940?
 
 
 # How much money was spent on Food and Tobacco in 1960
-
+food.1960 <- us.exp['Food and Tobacco', 'X1960']
 
 # What was the highest expenditure category in 1960?
-
+highest.1960 <- us.exp[us.exp$X1960 == max(us.exp$X1960), 'category']
 
 ### Bonus ###
 
 # Write a function that takes in a year as a parameter, and 
 # returns the highest spending category of that year
+DetectHighest <- function(year){
+  return (us.exp$category[us.exp[year] == max(us.exp[year])])
+}
 
 # Using your function, determine the highest spending category of each year
-
+highest.1950 <- DetectHighest('X1950')
 
 # Write a loop to cycle through the years, and store the highest spending category of
 # each year in a list
--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -2,34 +2,36 @@
 
 # Read data into a variable called `grants` using the `read.csv` function
 # Make sure *not* to read the strings as factors
+grants <- read.csv('data/gates_money.csv', stringsAsFactors = FALSE)
 
 # Use the View function to look at your data
 
 
 # Create a variable `spending` as the `total_amount` column of the dataset
-
+spending <- grants$total_amount
 
 # Confirm that your `spending` variable is a vector using the `is.vector` function
-
+is.vector('spending')
 
 # Create a variable `org` as the `organization` column of the dataset.
-
+org <- grants$organization
 
 ### Now you can ask some more interesting questions about the dataset.  Store your answers in variables ###
 
 # What was the mean grant value?
-
+mean <- mean(spending)
 
 # What was the dollar amount of the largest grant?
-
+max <- max(spending)
 
 # What was the dollar amount of the smallest grant?
-
+min <- min(spending)
 
 # Which organization received the largest grant?
-
+max.organization <- org[spending == max]
 
 # Which organization received the smallest grant?
-
+min.organization <- org[spending == min]
 
 # How many grants were awarded in 2010?
+grant.in.2010 <- length(spending[grants$start_year == 2010])

a7498a6e257fc830f1f4de5eb850ef01592066a0
Anni Yan <Anni Yan>
2017-04-11 14:18:19 -0700

    this is hard

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,20 +2,24 @@
 
 # Create a vector of the number of points the Seahawks scored the first 4 games of last season
 # Hint: (google "Seahawks scores 2016")
-
+scored <- c(36, 10, 10, 3)
+  
 # Create a vector of the number of points the Seahawks have allowed to be scored against them in the first 4 games
-
+be.scored <- c(6, 9, 12, 9)
 
 # Combine your two vectors into a dataframe
-
+seahawks <- data.frame(scored, be.scored)
 
 # Create a new column "diff" that is the difference in points
-
+difference <- scored - be.scored
+seahawks$diff <- difference
 
 # Create a new column "won" which is TRUE if the Seahawks wom
-
+won <- c(difference > 0)
+seahawks$won <- won
 
 # Create a vector of the opponents
-
+opponents <- c("cardinals", "vikings", "Dolphins", "Rams")
 
 # Assign your dataframe rownames of their opponents
+rownames(seahawks) <- opponents
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,27 +1,39 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100)
-
+employee <- c(1:100)
 
 # Create a vector of 2014 salaries using the runif function
-
+salarie <- runif(employee, min = 0, max = 1000)
 
 # Create a vector of 2015 salaries that are typically higher than the 2014 salaires (use runif again)
-
+salariesTwo <- runif(employee, min = 10, max = 2000)
 
 # Create a data.frame 'salaries' by combining the vectors you just made
-
+salaries <- data.frame(employee, salarie, salariesTwo)
 
 # Create a column 'raise' that stores the size of the raise between 2014 and 2015
-
+size <- c(salariesTwo - salarie)
+salaries$size <- size
 
 # Create a column 'got.raise' that is TRUE if the person got a raise
-
+got.raise <- c(size > 0)
+salaries$got.raise <- got.raise
 
 # Retrieve values from your data frame to answer the following questions:
 
 # What was the 2015 salary of employee 57
-
+print (salaries$salariesTwo[[57]])
 
 # How many employees got a raise?
+
+raised <- 0
+times <- 0
+while (times < 100){
+  times <- times + 1
+  if (salaries$got.raise == TRUE){
+    raised <- raised + 1
+  }
+  print (raised)
+}
 
 
 # What was the value of the highest raise?
